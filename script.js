f3a578bdf321a5590508ef2c42be0f2434296487
Sarthak Kapila <aniki123.ai@gmail.com>
2025-11-13 17:44:52 +0530

    --init

---
+++ script.js
@@ -0,0 +1,48 @@
+// NOTE: the javascript displays and update time and date (every 5 seconds)
+
+let initialDt = new Date(); // intitial time, displayed when the page is first loaded
+let dateTime = document.querySelector(".dateTime");
+
+// formatting time: displays it as "DD.MM.YYYY - HH:mm"
+// I chose not to install any dependency to keep the file lightweight, but you can install moment if you want to edit the time/date more easily
+dateTime.innerHTML =
+  ("0" + initialDt.getDate()).slice(-2) +
+  "." +
+  ("0" + (initialDt.getMonth() + 1)).slice(-2) +
+  "." +
+  initialDt.getFullYear() +
+  "&nbsp; - &nbsp;" +
+  ("0" + initialDt.getHours()).slice(-2) +
+  ":" +
+  ("0" + initialDt.getMinutes()).slice(-2);
+
+
+// updates time every 5 seconds
+setInterval(() => {
+  let updatedDt = new Date();
+  dateTime.innerHTML =
+    ("0" + updatedDt.getDate()).slice(-2) +
+    "." +
+    ("0" + (updatedDt.getMonth() + 1)).slice(-2) +
+    "." +
+    updatedDt.getFullYear() +
+    "&nbsp; - &nbsp;" +
+    ("0" + updatedDt.getHours()).slice(-2) +
+    ":" +
+    ("0" + updatedDt.getMinutes()).slice(-2);
+}, 5_000);
+
+const themeToggle = document.getElementById('theme-toggle');
+      const body = document.body;
+
+      themeToggle.addEventListener('click', () => {
+          if (body.classList.contains('dark-theme')) {
+              body.classList.remove('dark-theme');
+              body.classList.add('light-theme');
+              themeToggle.textContent = '⚫️';
+          } else {
+              body.classList.remove('light-theme');
+              body.classList.add('dark-theme');
+              themeToggle.textContent = '⚪️';
+          }
+      });
